9e314168cbc53e9ced14840c9f5d7410a3e6e295
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2025-09-04 06:01:39 +0530

    Time: 1 ms (71.48%) | Memory: 45.7 MB (47.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,34 +1,47 @@
-class Pair{
+class Pair {
     int row;
     int col;
-    Pair(int row, int col){
+
+    Pair(int row, int col) {
         this.row = row;
         this.col = col;
     }
 }
+
 class Solution {
     public void setZeroes(int[][] matrix) {
         Queue<Pair> q = new LinkedList<>();
 
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    q.add(new Pair(i,j));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    q.add(new Pair(i, j));
                 }
             }
         }
-        while(!q.isEmpty()){
+
+        int[] rowVisited = new int[m];
+        int[] colVisited = new int[n];
+        while (!q.isEmpty()) {
             Pair it = q.poll();
             int row = it.row;
             int col = it.col;
-            for(int j=0;j<n;j++){
-                matrix[row][j] = 0;
+
+            if (rowVisited[row] == 0) {
+                for (int j = 0; j < n; j++) {
+                    matrix[row][j] = 0;
+                }
+                 rowVisited[row] = 1;
             }
-            for(int i=0;i<m;i++){
-                 matrix[i][col] = 0;
+            if (colVisited[col] == 0) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][col] = 0;
+                }
+                 colVisited[col] = 1;
             }
         }
+
     }
 }
