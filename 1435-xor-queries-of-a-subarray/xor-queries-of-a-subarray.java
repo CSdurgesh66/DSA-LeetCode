2eea37d8e472bbb7979b404a41078bc232d3552d
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2025-10-12 05:45:43 +0530

    Time: 4 ms (20.13%) | Memory: 60.5 MB (66.00%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,21 +1,24 @@
 class Solution {
-    public int findXor(int[] arr, int left, int right){
+    public void findXor(int[] arr,int[] prefixXor){
          int n = arr.length;
          int xor = 0;
-         for(int i=left;i<=right;i++){
-            xor ^= arr[i];
+         for(int i=0;i<n;i++){
+            prefixXor[i+1] = prefixXor[i] ^ arr[i];
          }
-         return xor;
+         return;
     }
     public int[] xorQueries(int[] arr, int[][] queries) {
         int n = arr.length;
         int qn = queries.length;
         int[] ans = new int[qn];
+        int[] prefixXor = new int[n+1];
+        prefixXor[0] = 0;
+        findXor(arr,prefixXor);
+
         for(int i=0;i<qn;i++){
             int left = queries[i][0];
             int right = queries[i][1];
-            int xor = findXor(arr,left,right);
-            ans[i] = xor;
+            ans[i] = prefixXor[right+1] ^ prefixXor[left];
         }
         return ans;
     }
