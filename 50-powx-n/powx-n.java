104815897443028f7133812d79767975b10caaee
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2025-10-17 09:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (99.16%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static double helper(double x, long n){
+        if(n==0) return 1.0;
+        double half= helper(x,n/2);
+        return (n%2==0)? half*half: half*half*x;
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return helper(x, N);
+
+    }
+}
