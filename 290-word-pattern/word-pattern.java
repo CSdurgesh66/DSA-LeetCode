387884fbe190773f160e5b68ec63f65e877decb9
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2025-11-02 00:01:25 +0530

    Time: 2 ms (14.18%) | Memory: 42.8 MB (5.01%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String> map = new HashMap<>();
+        HashSet<String> used = new HashSet<>();
         String[] arr = s.trim().split("\\s+");
         int i = 0;
         int n = pattern.length();
         if(n!=arr.length) return false;
         for(String str:arr){
-            if(i>=n){
-                return true;
-            }
-            if(map.containsKey(pattern.charAt(i))){
-                if(!str.equals(map.get(pattern.charAt(i)))){
+           char ch = pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!str.equals(map.get(ch))){
                     return false;
                 }
             }else{
-                if(map.containsValue(str)){
+                if(used.contains(str)){
                     return false;
                 }
-                map.put(pattern.charAt(i),str);
+                map.put(ch,str);
+                used.add(str);
             }
             i++;
         }
