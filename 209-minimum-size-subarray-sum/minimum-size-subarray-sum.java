7ed0049e3e5204238c25142af3296df36cbc704a
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2026-02-13 07:37:07 +0530

    Time: 1 ms (99.68%) | Memory: 62.4 MB (12.78%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -6,15 +6,10 @@ class Solution {
         int sum = 0;
         while (r < n) {
             sum += nums[r];
-            if (sum >= target) {
-                int len = r - l + 1;
-                min = Math.min(min, len);
-            }
-            while(l<r && (sum-nums[l]) >= target){
+            while (sum >= target) {
+                min = Math.min(min, r - l + 1);
                 sum -= nums[l];
                 l++;
-                int len = r - l + 1;
-                min = Math.min(min, len);
             }
             r++;
         }
