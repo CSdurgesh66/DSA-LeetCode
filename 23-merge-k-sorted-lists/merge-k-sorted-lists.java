aa91dd4c73fb7feb0b76ced040470bc785d36126
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2026-02-15 07:41:55 +0530

    Time: 1 ms (99.71%) | Memory: 46.7 MB (86.08%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,32 +9,56 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        ListNode ans = null;
-        if(lists == null || n == 0) return ans;
-
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
-
-        for(ListNode node:lists){
-            if(node!=null) pq.add(node);
-        }
-
+    public ListNode merge(ListNode A,ListNode B){
         ListNode curr = new ListNode(100);
         ListNode head = curr;
 
-        while(!pq.isEmpty()){
-            ListNode minNode = pq.poll();
-
-            curr.next = minNode;
-            curr = curr.next;
-            minNode = minNode.next;
-            if(minNode!=null){
-                pq.add(minNode);
+        while(A!=null && B!=null){
+            if(A.val < B.val){
+                curr.next = A;
+                A = A.next;
+                curr = curr.next;
+            }else {
+                curr.next = B;
+                B = B.next;
+                curr = curr.next;
             }
         }
 
-      
+        while(A!=null){
+            curr.next = A;
+            A = A.next;
+            curr = curr.next;
+        }
+
+        
+        while(B!=null){
+            curr.next = B;
+            B= B.next;
+            curr = curr.next;
+        }
+
         return head.next;
     }
+
+    public ListNode divide(ListNode[] lists, int left, int right){
+        if(left==right){
+            return lists[left];
+        }
+
+        int mid = left + (right-left)/2;
+
+        ListNode l1 = divide(lists,left,mid);
+        ListNode l2 = divide(lists,mid+1,right);
+
+        return merge(l1,l2);
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if(lists==null || n==0) return null;
+
+        return divide(lists,0,n-1);
+       
+    }
+
 }
