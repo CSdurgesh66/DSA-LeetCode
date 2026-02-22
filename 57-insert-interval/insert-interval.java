6ef27d11973f2c95fe31e97c9994b2651f3c6013
durgesh kumar <122608416+CSdurgesh66@users.noreply.github.com>
2026-02-22 07:07:36 +0530

    Time: 1 ms (97.66%) | Memory: 47.2 MB (34.92%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,43 +1,28 @@
 class Solution {
-    public int[][] merge(ArrayList<int[]> list){
-        int n = list.size();
-        ArrayList<int[]> ans = new ArrayList<>();
-        int[] firstInterval = list.get(0);
-        int prevst = firstInterval[0];
-        int prevend = firstInterval[1];
-        for(int i=1;i<n;i++){
-            int[] curr = list.get(i);
-
-            if(prevend >= curr[0]){
-                prevend = Math.max(prevend,curr[1]);
-            }else {
-                ans.add(new int[]{prevst,prevend});
-                prevst = curr[0];
-                prevend = curr[1];
-            }
-        }
-        ans.add(new int[]{prevst,prevend});
-        return ans.toArray(new int[ans.size()][]);
-    }
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> list = new ArrayList<>();
         int n = intervals.length;
-        if(n==0){
-            list.add(newInterval);
-            return list.toArray(new int[list.size()][]);
-        }
-        boolean first = true;
-        for(int i=0;i<n;i++){
-            if(intervals[i][0]>newInterval[0] && first){
-                list.add(newInterval);
-                first = false;
-            }
+        int i = 0;
+
+        while(i<n && intervals[i][1] < newInterval[0]){
             list.add(intervals[i]);
+            i++;
         }
-        if(first){
-            list.add(newInterval);
-            first = false;
+
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1],intervals[i][1]);
+            i++;
         }
-        return merge(list);
+
+        list.add(newInterval);
+
+
+        while(i<n){
+            list.add(intervals[i]);
+            i++;
+        }
+
+        return list.toArray(new int[list.size()][]);
     }
 }
